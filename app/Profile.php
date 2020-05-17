58931e586ca23b0f48b3750c724fe779ef739d0c
kodai1419 <kawano.kodai@gmail.com>
2020-05-17 18:25:29 +0900

    20200517_commit

---
+++ app/Profile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    //
+        protected $guarded = array('id');
+
+    // 以下を追記
+    public static $rules = array(
+        'firstname' => 'required',
+        'lastname' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+        
+    );
+
+}
